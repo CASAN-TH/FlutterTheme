6c15be8676ec1abba31b2669ec856961ca54ea4f
thanayutPhi <thanayut.phi@outlook.com>
2020-07-02 17:16:37 +0700

    welcome

---
+++ lib/welcome/body.dart
@@ -0,0 +1,62 @@
+import 'package:FkutterTestapp/constants.dart';
+import 'package:FkutterTestapp/coomponents/rounded_button.dart';
+import 'package:FkutterTestapp/login/login_screen.dart';
+import 'package:FkutterTestapp/signup/callbody.dart';
+import 'package:FkutterTestapp/welcome/background.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "WELCOME TO EDU",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.05),
+            SvgPicture.asset(
+              "assets/icons/welcome-new.svg",
+              height: size.height * 0.45,
+            ),
+            SizedBox(height: size.height * 0.05),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            RoundedButton(
+              text: "SIGN UP",
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
