db5e285f92d34c93d2e8a981e7d08c45630b9aaf
thanayutPhi <thanayut.phi@outlook.com>
2020-07-02 17:12:37 +0700

    login

---
+++ lib/login/login_screen.dart
@@ -0,0 +1,13 @@
+// import 'package:FlutterLogin/login/components/body.dart';
+import 'package:FkutterTestapp/login/body.dart';
+import 'package:flutter/material.dart';
+// import 'package:flutter_auth/Screens/Login/components/body.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
